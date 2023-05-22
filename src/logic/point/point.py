d5ce6e9268d35507beecccfe5af34ec25707f0ff
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-05-22 17:51:45 +0700

    fix: point ge and le overloading return values

--- src/logic/point/point.py
+++ src/logic/point/point.py
@@ -16,13 +16,13 @@ class Point:
         return self.x > other.x and self.y > other.y
 
     def __ge__(self, other):
-        return self > other or self == other
+        return self.x >= other.x and self.y >= other.y
 
     def __lt__(self, other):
         return self.x < other.x and self.y < other.y
 
     def __le__(self, other):
-        return self < other or self == other
+        return self.x <= other.x and self.y <= other.y
 
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
